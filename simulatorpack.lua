31b5497b753185acd9b827b0c910f07e038442da
NightsTimeZ <84789294+NightsTimeZ@users.noreply.github.com>
2024-03-26 20:15:15 +0700

    dfsfdsadasf

--- simulatorpack.lua
+++ simulatorpack.lua
@@ -1,1 +1,12 @@
-game.Players.LocalPlayer:Kick("Pls Wait For v2")
+    local gamelist = {
+        [1511883870]  = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/Shindo.lua", -- Check Shin
+    }
+    local checkgame = gamelist[game.gameId]
+
+    if gamelist[game.gameId] then
+        print (checkgame)
+        loadstring(game:HttpGet(checkgame))()
+    else 
+        game.Players.LocalPlayer:Kick("Game Not Support")
+
+    end

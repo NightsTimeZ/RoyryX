81b8c12117edd8aa1cb5b03065314f93f260b29e
NightsTimeZ <84789294+NightsTimeZ@users.noreply.github.com>
2024-04-04 00:31:16 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -29,6 +29,10 @@ else
         [994732206] = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/MainBF.lua",
         [4777817887] = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/BladeBall.lua",
         [1511883870]  = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/Shindo.lua",
+        [1202096104] = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/DrivingEmpire.lua",
+        [1202096104] = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/DrivingEmpire2.lua",
+        [1202096104] = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/DrivingEmpire3.lua",
+        [4093155512] = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/SwordBurst3.lua",
     }
     local checkgame = gamelist[game.gameId]
 

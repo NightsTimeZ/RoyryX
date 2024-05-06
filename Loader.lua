d120408340cb555b85ae7b2709100568054bce44
NightsTimeZ <84789294+NightsTimeZ@users.noreply.github.com>
2024-05-06 21:33:21 +0700

    sad

--- Loader.lua
+++ Loader.lua
@@ -31,7 +31,8 @@ else
         [1511883870]  = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/Shindo.lua",
         [1202096104] = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/DrivingEmpire.lua",
         [4093155512] = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/SwordBurst3.lua",
-        [5361032378] = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/Sols-RNG.lua"
+        [5361032378] = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/Sols-RNG.lua",
+[1451439645] = "https://raw.githubusercontent.com/markxd07/V2-Scripts/main/KL.lua"
     }
     local checkgame = gamelist[game.gameId]
 
